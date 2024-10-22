2801f21ba3a6f79c780390a6668c3156846fadf9
KriddMy <kriddmy@gmail.com>
2024-10-22 18:24:58 +0300

    init

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -4,17 +4,20 @@
 
 int main()
 {
-    std::cout << "Starting server on port: " << SERVER_PORT << "." << std::endl;
+    try {
+        std::cout << "Starting server on port: " << SERVER_PORT << "." << std::endl;
     
-    PacketReciever server(1000); // 1 mb/s
-    if(server.open(SERVER_PORT) < 0) {
-        std::cerr << "Server: Failed to open connection.";
-        return 1;
-    }
+        PacketReciever server(1000); // 1 mb/s
+        if(server.open(SERVER_PORT) < 0) {
+            std::cerr << "Server: Failed to open connection.";
+            return 1;
+        }
 
-    while(true) {
-        server.acceptPacket(10); //10 sec
-    }
+        while(true) {
+            server.acceptPacket(10); //10 sec
+        }
+    } catch(...) { std::cerr << "Server: Unknown Error Occured... Exiting"; }
+    
 
     return 0;
 }
